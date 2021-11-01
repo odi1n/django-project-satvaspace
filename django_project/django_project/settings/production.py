a2237cb77944ac5362b8917fb0a1534ca8abd8f4
odi1n <sashf1999@mail.ru>
2021-11-01 23:53:42 +0300

    Вынес .env Обновил README.md

--- django_project/django_project/settings/production.py
+++ django_project/django_project/settings/production.py
@@ -13,6 +13,6 @@ DATABASES = {
         'USER': env.str("POSTGRES_USER"),
         'PASSWORD': env.str("POSTGRES_PASSWORD"),
         'HOST': env.str("POSTGRES_HOST"),
-        'POST': env.str("POSTGRES_POST"),
+        'PORT': env.str("POSTGRES_PORT"),
     }
 }
